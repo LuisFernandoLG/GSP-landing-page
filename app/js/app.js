1a9c28521b2a17c69a654fd37e7b9adf1d6a1f0a
Luis Lopez <luislopez1099lg@gmail.com>
2021-07-24 19:01:51 -0600

    menu float works!

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,28 @@
 import { setLazyLoad } from "./components/LazyLoad.js";
+// import { setLazyLoad } from "./components/MenuButton";
 
 addEventListener("DOMContentLoaded", () => {
   setLazyLoad(true);
+});
+
+const $menuBtn = document.querySelector(".menu");
+const $closeMenuBtn = document.querySelector(".nav__close-btn");
+const $nav = document.querySelector(".nav");
+
+const $body = document.body;
+
+$menuBtn.addEventListener("click", () => {
+  $nav.classList.add("show");
+});
+
+$closeMenuBtn.addEventListener("click", (e) => {
+  $nav.classList.remove("show");
+});
+
+$body.addEventListener("click", (e) => {
+  const classes = e.target.classList;
+
+  if (classes.contains("nav__link")) {
+    $nav.classList.remove("show");
+  }
 });
